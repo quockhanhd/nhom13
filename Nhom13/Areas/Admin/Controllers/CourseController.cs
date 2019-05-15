870287267583839d393a2d96fa1e3ee3ccf09f12
khanh <quockhanhitdlu@gmail.com>
2019-05-15 09:42:02 +0700

    add quan ly course

--- Nhom13/Areas/Admin/Controllers/CourseController.cs
+++ Nhom13/Areas/Admin/Controllers/CourseController.cs
@@ -1,17 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Model.EF;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace Nhom13.Areas.Admin.Controllers
 {
     public class CourseController : Controller
     {
-        // GET: Admin/Course
+        OnlineCourse db = new OnlineCourse();
         public ActionResult Index()
         {
             return View();
         }
+        public JsonResult GetCourseList()
+        {
+            return Json(new { data = db.Courses.ToList() });
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public JsonResult AddCourse()
+        {
+            var countform = Request.Form.Count;
+            var countfile = Request.Files.Count;
+            var name = Request.Form["Ten"].ToString();
+            var gia = Request.Form["Gia"].ToString();
+            var anh = Request.Files["Anh"];
+            var motaRaw = Request.Form["Mota"];
+            var mota = HttpUtility.UrlDecode(motaRaw);
+            var path = Path.Combine(Server.MapPath("~/Image"), anh.FileName);
+
+            anh.SaveAs(path);
+
+            Course course = new Course();
+            course.Name = name;
+            course.Image = anh.FileName;
+            course.Price = decimal.Parse(gia);
+
+            course.Description = mota;
+            db.Courses.Add(course);
+            db.SaveChanges();
+            return Json(new { data = true });
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public JsonResult EditCourse()
+        {
+            var countform = Request.Form.Count;
+            var countfile = Request.Files.Count;
+            var name = Request.Form["Ten"].ToString();
+            var gia = Request.Form["Gia"].ToString();
+            var anh = Request.Files["Anh"];
+            var motaRaw = Request.Form["Mota"];
+            var mota = HttpUtility.UrlDecode(motaRaw);
+            var path = Path.Combine(Server.MapPath("~/Image"), anh.FileName);
+            var id = int.Parse(Request.Form["ID"]);
+            anh.SaveAs(path);
+
+            Course course = db.Courses.Single(x => x.ID == id);
+            course.Name = name;
+            course.Image = anh.FileName;
+            course.Price = decimal.Parse(gia);
+
+            course.Description = mota;
+            db.SaveChanges();
+            return Json(new { data = true });
+        }
+        public JsonResult DeleteCourse(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var course = db.Courses.Single(x => x.ID == id);
+                db.Courses.Remove(course);
+                db.SaveChanges();
+                return Json(new { data = true });
+            }
+            else
+                return Json(new { data = false });
+        }
+        [HttpGet]
+        public JsonResult LoadCategory()
+        {
+            return Json(new { data = db.Course_Category.ToList() }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AddandEditCourseModal(int id)
+        {
+            var result = new Course();
+            string mode = "Add";
+            result = db.Courses.FirstOrDefault(x => x.ID == id);
+            if (result == null)
+            {
+                mode = "Add";
+                result = new Course();
+            }
+            else
+            {
+                mode = "Edit";
+            }
+            ViewData["Mode"] = mode;
+            ViewData["Obj"] = result;
+            return View();
+        }
+
     }
 }
--- Nhom13/Areas/Admin/Controllers/UserController.cs
+++ Nhom13/Areas/Admin/Controllers/UserController.cs
@@ -16,27 +16,92 @@ namespace Nhom13.Areas.Admin.Controllers
             return View();
         }
 
-        public JsonResult GetCartegoryList()
+        public JsonResult GetUserList()
         {
-            return Json(new { data = db.Course_Category.ToList() });
+            var listUser = db.Users.ToList();
+            return Json(new { data = listUser });
         }
 
-        [HttpPost, ValidateInput(false)]
-        public JsonResult AddCartegory(string name, string des)
+        public JsonResult AddUser(string userN, string pass, string name, string email)
         {
             if (ModelState.IsValid)
             {
-                Course_Category cartegory = new Course_Category();
-                cartegory.Name = name;
-                cartegory.Description = des;
-                cartegory.CreatedDate = DateTime.Now;
-                db.Course_Category.Add(cartegory);
+                User user = new User();
+                user.UserName = userN;
+                user.Password = pass;
+                user.Name = name;
+                user.Email = email;
+                user.GroupID = "1";
+                user.Status = true;
+                db.Users.Add(user);
                 db.SaveChanges();
-                return Json(new { });
+                return Json(new { data = true });
+            }
+            else
+            {
+                return Json(new { data = true });
+            }
+        }
+        public JsonResult EditUser(int id, string userN, string pass, string name, string email, string group)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = db.Users.Single(x => x.ID == id);
+                user.UserName = userN;
+                user.Password = pass;
+                user.Name = name;
+                user.Email = email;
+                user.GroupID = group;
+                user.Status = true;
+                try
+                {
+                    db.SaveChanges();
+                    return Json(new { data = true });
+                }
+                catch (Exception)
+                {
+                    return Json(new { data = false });
+                }
+            }
+            else
+            {
+                return Json(new { data = true });
+            }
+        }
+        public JsonResult DeleteUser(int id)
+        {
+            User hs = db.Users.Single(x => x.ID == id);
+            if (hs != null)
+            {
+                db.Users.Remove(hs);
+                db.SaveChanges();
+                return Json(new { data = true });
             }
             else
                 return Json(new { data = false });
-
+        }
+        [HttpGet]
+        public JsonResult LoadGroup()
+        {
+            return Json(new { data = db.UserGroups.ToList() }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult AddandEditUserModal(int id)
+        {
+            var result = new User();
+            string mode = "Add";
+            result = db.Users.FirstOrDefault(x => x.ID == id);
+            if (result == null)
+            {
+                mode = "Add";
+                result = new User();
+            }
+            else
+            {
+                mode = "Edit";
+            }
+            ViewData["Mode"] = mode;
+            ViewData["Obj"] = result;
+            return View();
         }
     }
 }
