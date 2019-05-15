777cd963d1f7e7a59f5f27138aad2dac875360f5
Dinh Quoc Khanh <quockhanhitdlu@gmail.com>
2019-05-15 07:15:39 +0700

    1

--- Nhom13/Areas/Admin/Controllers/CourseController.cs
+++ Nhom13/Areas/Admin/Controllers/CourseController.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Model.EF;
+
 namespace Nhom13.Areas.Admin.Controllers
 {
-    public class UserController : Controller
+    public class CourseController : Controller
     {
-        OnlineCourse db = new OnlineCourse();
-
+        // GET: Admin/Course
         public ActionResult Index()
         {
-
             return View();
         }
-
-    
     }
 }
